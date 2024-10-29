f5c6da04734cb2fe952ebe9d13e51e0deab0ff8a
zhaoyiyidan <139891330+zhaoyiyidan@users.noreply.github.com>
2024-10-29 18:38:11 +0800

    complete the semantic part

--- Semantic/Analysis.cpp
+++ Semantic/Analysis.cpp
@@ -2,10 +2,58 @@
 // Created by 赵鲲翔 on 2024/10/28.
 //
 #include "Analysis.h"
-
+#include "../Syntax/header/ALLHEADER.h"
 void Analysis::visit(const FunctionDec &node) {
-    node.accept(*this);
+    node.body->accept(*this);
 }
 void Analysis::visit(const compoundstmt &node) {
+    for (auto &stmt: node.stmts) {
+       if (stmt->GetNodeType()=="compoundstmt"){
+        symbolTable.EnterScope();
+        stmt->accept(*this);
+        symbolTable.ExitScope();
+       }
+       else{
+              stmt->accept(*this);
+       }
+    }
+}
+void Analysis::visit(const VarDecl &node) {
+    auto type=node.type->GetNodeType();
+    auto VarDef=dynamic_cast<class VarDef*>(node.VarDef.get());
+   if (!symbolTable.ExistSymbol(VarDef->identifier)){
+       symbolTable.InsertSymbol(VarDef->identifier, type,1);
+   }
+   else{
+       std::cerr<<"Error: Variable "<<VarDef->identifier<<" has been declared before"<<std::endl;
+   }
+    for (auto &VarDef: node.VarDefs) {
+        auto VarDef_=dynamic_cast<class VarDef*>(VarDef.get());
+        if (!symbolTable.ExistSymbol(VarDef_->identifier)){
+            symbolTable.InsertSymbol(VarDef_->identifier, type,1);
+        }
+        else{
+            std::cerr<<"Error: Variable "<<VarDef_->identifier<<" has been declared before"<<std::endl;
+        }
+    }
 
+}
+void Analysis::visit(const ConstDecl &node) {
+    auto type=node.type->GetNodeType();
+    auto VarDef=dynamic_cast<class ConstDef*>(node.ConstDef.get());
+    if (!symbolTable.ExistSymbol(VarDef->identifier)){
+        symbolTable.InsertSymbol(VarDef->identifier, type,0);
+    }
+    else{
+        std::cerr<<"Error: Variable "<<VarDef->identifier<<" has been declared before"<<std::endl;
+    }
+    for (auto &VarDef: node.ConstDefs) {
+        auto VarDef_=dynamic_cast<class ConstDef*>(VarDef.get());
+        if (!symbolTable.ExistSymbol(VarDef_->identifier)){
+            symbolTable.InsertSymbol(VarDef_->identifier, type,0);
+        }
+        else{
+            std::cerr<<"Error: Variable "<<VarDef_->identifier<<" has been declared before"<<std::endl;
+        }
+    }
 }
---
+++ Syntax/header/ALLHEADER.h
@@ -0,0 +1,27 @@
+//
+// Created by 赵鲲翔 on 2024/10/29.
+//
+
+#ifndef COMPILER_ALLHEADER_H
+#define COMPILER_ALLHEADER_H
+#include "Declaration/FunctionDec.h"
+#include "statement/compoundstmt.h"
+#include "statement/ReturnStmt.h"
+#include "statement/AssignStmt.h"
+#include "statement/Expression.h"
+#include "statement/LOrExp.h"
+#include "statement/LAndExp.h"
+#include "statement/EqExp.h"
+#include "statement/RelExp.h"
+#include "statement/AddExp.h"
+#include "statement/MulExp.h"
+#include "statement/UnaryExp.h"
+#include "statement/PrimaryExp.h"
+#include "statement/UnaryOp.h"
+#include "Declaration/ConstDecl.h"
+#include "Declaration/VarDecl.h"
+#include "Declaration/ConstDef.h"
+#include "Declaration/VarDef.h"
+#include "Type/IntegerLiteral.h"
+
+#endif //COMPILER_ALLHEADER_H
