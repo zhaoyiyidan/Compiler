d10ed406faac7b903d19e8b62db67e1b237d8ba7
A <A>
2024-11-20 18:10:03 +0800

    fix bug of struct decl

--- Test/source1.cpp
+++ Test/source1.cpp
@@ -12,9 +12,9 @@ int mains(int a,int b){
     int e=3;
     int f=4;
     int c=33;
-    int x[2]={3,4},b[3]={1,2,3},k=3;
-    int a=1;
-
+    int x[2]={3,4},h[3]={1,2,3},k=3;
+    int t=1;
+    student s;  
 }
 double aaaaa(int d,bool c){
     char kkk=888;
--- main.cpp
+++ main.cpp
@@ -4,8 +4,8 @@
  #include "Semantic/APIOfSemantic.h"
 int main(){
     // you need absolute path to use gettoken
-    auto token=gettoken("/Users/atri/CLionProjects/Compiler/Test/Functioncall.cpp");
-    auto tree= buildASTTree(token);
+    auto token=gettoken("/Users/atri/CLionProjects/Compiler/Test/struct.cpp");
+    auto tree= buildASTTree(token);// it do not support struct now
     auto sym=analysis(tree);
     tree.getNode();
         }
